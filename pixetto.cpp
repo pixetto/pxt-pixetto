6a0de0b326d889e043b2a1d078e76835bbccab29
Sylvia Lin <SylviaLin@via.com.tw>
2020-10-12 17:11:32 +0800

    1. Reset pixetto if no data returns in isDetected(). 2. Support Digits Operation function.

--- pixetto.cpp
+++ pixetto.cpp
@@ -8,9 +8,13 @@
 #define PXT_CMD_STREAMOFF	0x7A
 #define PXT_CMD_STREAMON_CB 0x7B
 #define PXT_CMD_QUERY		0x7C
+#define PXT_CMD_RESET		0x53 //(83)
 
 #define PXT_RET_CAM_SUCCESS	0xE0
 #define PXT_RET_CAM_ERROR	0xE1
+
+#define SERIAL_BUF_SIZE		64
+#define DATA_SIZE			33
 
 enum PixSerialPin {
     P0 = 0,
@@ -27,7 +31,7 @@ enum PixSerialPin {
 enum PixFunction {
         //% block="Color Detection"
         COLOR_DETECTION=1,
-        //% block="Color Label Detection"
+        //% block="Color Codes Detection"
         COLOR_LABEL=2,
         //% block="Shape Detection"
         SHAPE_DETECTION=3,
@@ -38,7 +42,9 @@ enum PixFunction {
         //% block="Keypoint"
         KEYPOINT=8,
         //% block="Neural Network"
-        NEURAL_NETWORK=15,
+        NEURAL_NETWORK=9,
+        //% block="AprilTag(16h5)"
+        APRILTAG=10,
         //% block="Face Detection"
         FACE_DETECTION=11,
         //% block="Traffic Sign Detection"
@@ -47,8 +53,20 @@ enum PixFunction {
         HANDWRITING_DIGITS_DETECTION=13,
         //% block="Handwriting Letters Detection"
         HANDWRITING_LETTERS_DETECTION=14,
-        //% block="AprilTag(16h5)"
-        APRILTAG=10
+        /*
+        //% block="Remote Computing"
+        REMOTE_COMPUTING=15,
+        //% block="Lanes Detection"
+        LANES_DETECTION=16,
+        */
+        //% block="Digits Operation"
+        DIGITS_OPERATION=17
+        /*
+        //% block="Simple Classifier"
+        SIMPLE_CLASSIFIER=18,
+        //% block="Voice Commands"
+        VOICE_COMMANDS=19
+        */
 };
     
 
@@ -57,7 +75,15 @@ using namespace pxt;
 namespace pixetto {
 
 	MicroBitSerial *serial = nullptr;
-	uint8_t data_buf[10] = {0xFF};
+	uint8_t data_buf[DATA_SIZE] = {0xFF};
+	int data_len = 0;
+	int m_x = 0;
+	int m_y = 0;
+	int m_w = 0;
+	int m_h = 0;
+	int m_eqLen = 0;
+	float m_eqAnswer = 0;
+	char m_eqExpr[17] = {0};
 	
     bool getPinName(PixSerialPin p, PinName& name) {
       switch(p) {
@@ -81,55 +107,65 @@ namespace pixetto {
 		for (uint8_t i=1; i<len-2; i++)
 			sum += buf[i];
 		
-		if (sum == PXT_PACKET_START || sum == PXT_PACKET_END)
+		if (sum == PXT_PACKET_START || sum == PXT_PACKET_END || sum == 0xFF)
 			sum = 0xAA;
 		
-		return (sum == buf[8]);
+		return (sum == buf[len-2]);
 	}
 
+	bool opencam(bool reset) 
+	{
+		if (reset)
+			uBit.sleep(6000);
+			
+		int try_streamon = 0;
+		do {
+			uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_STREAMON_CB, 0, PXT_PACKET_END};
+			serial->send(cmd_buf, 5);
+			
+			int read_len = 0;
+			int loop = 0;
+			uint8_t code_buf[5] = {0xFF};
+			do {
+				read_len = serial->read(code_buf, 1, ASYNC);
+				
+				if (read_len == 0 || read_len == MICROBIT_NO_DATA) {
+					loop++;
+				}
+			} while (code_buf[0] != PXT_PACKET_START && loop < 50000);
+			
+			if (read_len == 0 || read_len == MICROBIT_NO_DATA) break;
+				
+			read_len = serial->read(&code_buf[1], 4);
+
+			if (code_buf[0] == PXT_PACKET_START &&
+				code_buf[4] == PXT_PACKET_END &&
+				code_buf[2] == PXT_RET_CAM_SUCCESS)
+				return true;
+			try_streamon++;
+			uBit.sleep(500);
+		} while (try_streamon < 4);
+		
+		return false;
+	}
     //% 
     bool begin(PixSerialPin rx, PixSerialPin tx){
+		int ret = false;
 		PinName txn, rxn;
 		uBit.sleep(8000);
 		if (getPinName(tx, txn) && getPinName(rx, rxn))
 		{
-			serial = new MicroBitSerial(txn, rxn);
+			serial = new MicroBitSerial(txn, rxn, 64, 20);
 			serial->baud(38400);
 			//serial->setRxBufferSize(64);
 			//serial->setTxBufferSize(32);
 			uBit.sleep(100);
 			
-			int try_streamon = 0;
-			do {
-				uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_STREAMON_CB, 0, PXT_PACKET_END};
-				serial->send(cmd_buf, 5);
-				
-				int read_len = 0;
-				int loop = 0;
-				uint8_t code_buf[5] = {0xFF};
-				do {
-					read_len = serial->read(code_buf, 1, ASYNC);
-					
-					if (read_len == 0 || read_len == MICROBIT_NO_DATA) {
-						loop++;
-					}
-				} while (code_buf[0] != PXT_PACKET_START && loop < 50000);
-				
-				if (read_len == 0) break;
-					
-				read_len = serial->read(&code_buf[1], 4);
-
-				if (code_buf[0] == PXT_PACKET_START &&
-					code_buf[4] == PXT_PACKET_END &&
-					code_buf[2] == PXT_RET_CAM_SUCCESS)
-					return true;
-				try_streamon++;
-				uBit.sleep(500);
-			} while (try_streamon < 4);
+			ret = opencam(false);
 		}
-		return false;
+		return ret;
     }
-    
+    /*
     //%
     bool isDetected(){
 		int read_len = 0;
@@ -144,13 +180,88 @@ namespace pixetto {
 		} while (data_buf[0] != PXT_PACKET_START);
 
 		read_len = serial->read(&data_buf[1], 9);
+		 
+		m_x = data_buf[4];
+		m_y = data_buf[5];
+		m_w = data_buf[6];
+		m_h = data_buf[7];
 		
 		if (read_len != 9) return false;
 		if (data_buf[9] != PXT_PACKET_END) return false;
 		if (!verifyChecksum(data_buf, 10)) return false;
 		if (data_buf[2] == 0) return false;
 		return true;
+	} */
+	//%
+    bool isDetected(){
+		int read_len = 0;
+		int loop = 0;
+		int a = 0;
+		for (a=0; a<DATA_SIZE; a++)
+			data_buf[a] = 0xFF;
+	
+		uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_QUERY, 0, PXT_PACKET_END};
+		serial->send(cmd_buf, 5);
+
+		do {
+			read_len = serial->read(data_buf, 1, ASYNC);
+			loop++;
+		} while (data_buf[0] != PXT_PACKET_START && loop < 50000);
+		
+		if (read_len == 0 || read_len == MICROBIT_NO_DATA) {
+			uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_RESET, 0, PXT_PACKET_END};
+			serial->send(cmd_buf, 5);
+			opencam(true);
+			return false;
+		}
+
+		read_len = serial->read(&data_buf[1], 2);//, ASYNC); // <len, func_id>
+		data_len = data_buf[1];
+		if (data_len > 3)
+			read_len = serial->read(&data_buf[3], data_len - 3);//, ASYNC);
+		else
+			return false;
+		
+		if (read_len != (data_len-3)) return false;
+		if (data_buf[data_len-1] != PXT_PACKET_END) return false;
+		if (!verifyChecksum(data_buf, data_len)) return false;
+		if (data_buf[2] == 0) return false; // num == 0
+		
+		if (data_buf[2] == DIGITS_OPERATION) {
+			m_x = data_buf[3];
+			m_y = data_buf[4];
+			m_w = data_buf[5];
+			m_h = data_buf[6];
+			
+			m_eqAnswer = 0;
+			for (a=8; a<=14; a++) 
+				m_eqAnswer = m_eqAnswer * 10 + data_buf[a];
+
+			m_eqAnswer /= 100;
+			if (data_buf[7] == 0) m_eqAnswer = 0 - m_eqAnswer;
+			
+			memset(m_eqExpr, 0, sizeof(m_eqExpr));
+			m_eqLen = data_len - 17;
+			for (a=0; a<m_eqLen; a++)
+				m_eqExpr[a] = (char)data_buf[a+15];
+		}
+		/*
+		else if (data_buf[2] == LANES_DETECTION) {
+		}
+		else if (data_buf[2] == APRILTAG) {
+		}
+		*/
+		else {
+			m_x = data_buf[4];
+			m_y = data_buf[5];
+			m_w = data_buf[6];
+			m_h = data_buf[7];
+		}
+		return true;
 	}
+
+
+
 	//%
 	bool getFuncID(int func_id){
 		return (data_buf[2] == func_id);
@@ -207,19 +318,30 @@ namespace pixetto {
 	
 	//%
 	int getPosX() {
-		return data_buf[4];
+		return m_x;
 	}
 	//%
 	int getPosY() {
-		return data_buf[5];
+		return m_y;
 	}
 	//%
 	int getWidth() {
-		return data_buf[6];
+		return m_w;
 	}
 	//%
 	int getHeight() {
-		return data_buf[7];
+		return m_h;
+	}
+
+	//%
+	float getEquationAnswer() {
+		return m_eqAnswer;
+	}
+
+	//%
+	String getEquationExpr() {
+		ManagedString s = m_eqExpr;
+		return PSTR(s);
 	}
 	
 }
