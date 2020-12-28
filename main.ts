5a5eb5ecb8662bdcfd7b92b51a8879057fb5bcfd
Sylvia Lin <SylviaLin@via.com.tw>
2020-12-28 17:31:39 +0800

    1. Support apriltag, lanes detection, remote computing, simple classifier 2. Compatible with makecode v3 (microbit v2). 3. Fix hang issue when initialziing Pixetto.

--- main.ts
+++ main.ts
@@ -38,7 +38,7 @@ namespace pixetto {
 		SIGN_WRONG_WAY,
 		//% block="No U turn"
 		SIGN_NO_U_TURN,
-		//% block="Maximum speed"
+		//% block="Maximum speed limit"
 		SIGN_MAX_SPEED,
 		//% block="One-way traffic"
 		SIGN_ONEWAY_TRAFFIC,
@@ -46,7 +46,7 @@ namespace pixetto {
 		SIGN_LEFT_TURN,
 		//% block="Right turn"
 		SIGN_RIGHT_TURN,
-		//% block="Minimum speed"
+		//% block="Minimum speed limit"
 		SIGN_MIN_SPEED,
 		//% block="U turn"
 		SIGN_U_TURN,
@@ -142,100 +142,212 @@ namespace pixetto {
 		Z
     }
       
+    export enum PixVoiceCommand {
+		//% block="你好"
+		VOICE_Hello = 1,
+		//% block="謝謝"
+		VOICE_Thanks,
+		//% block="再見"
+		VOICE_Bye,
+		//% block="這是甚麼"
+		VOICE_WhatsThis,
+		//% block="現在幾點"
+		VOICE_WhatTime,
+		//% block="我幾歲"
+		VOICE_HowOld,
+		//% block="今天星期幾"
+		VOICE_WhatDay,
+		//% block="講故事"
+		VOICE_TellStory,
+		//% block="說笑話"
+		VOICE_TellJoke,
+		//% block="念唐詩"
+		VOICE_ReadPoem,
+		//% block="開燈"
+		VOICE_TurnOnLight,
+		//% block="關燈"
+		VOICE_TurnOffLight,
+		//% block="左轉"
+		VOICE_TurnLeft,
+		//% block="右轉"
+		VOICE_TurnRight,
+		//% block="前進"
+		VOICE_GoAhead,
+		//% block="後退"
+		VOICE_MoveBack,
+		//% block="停止"
+		VOICE_Stop,
+		//% block="開啟"
+		VOICE_Open,
+		//% block="關閉"
+		VOICE_Close,
+		//% block="睜開眼睛"
+		VOICE_OpenEyes1,
+		//% block="睜眼"
+		VOICE_OpenEyes2,
+		//% block="閉上眼睛"
+		VOICE_CloseEyes1,
+		//% block="閉眼"
+		VOICE_CloseEyes2,
+		//% block="跳一下"
+		VOICE_Jump,
+		//% block="起立"
+		VOICE_StandUp,
+		//% block="蹲下"
+		VOICE_SquatDown
+    }
+    
 	//% block="initialize vision sensor RX %rx TX %tx"
 	//% shim=pixetto::begin
+	//% weight=100
 	export function begin(rx: PixSerialPin, tx: PixSerialPin): boolean {
 		return false
     }
     
     //% block="object is detected"
     //% shim=pixetto::isDetected
+    //% weight=95
     export function isDetected(): boolean {
 		return false;
     }
     
     //% block="current function is %func_id"
     //% shim=pixetto::getFuncID
+    //% weight=90
     export function getFuncID(func_id: PixFunction): boolean {
         return false;
     }
-    
-    //% block="Color Detection detects color %color"
-    //% shim=pixetto::get_colordetect_color
-    export function get_colordetect_color(color: PixColorType): boolean {
-        return false;
-    }
-    
-    //% block="Shape Detection detects shape %shape"
-    //% shim=pixetto::get_shapedetect_shape
-    export function get_shapedetect_shape(shape: PixShapeType): boolean {
-        return false;
-    }
 
-    //% block="Circle Detection detects color %color"
-    //% shim=pixetto::get_circledetect_color
-    export function get_circledetect_color(color: PixColorType): boolean {
-        return false;
-    }
-
-    //% block="Neural Network detects %id"
-    //% shim=pixetto::get_nn_id
-    export function get_nn_id(id: number): boolean {
-        return false;
-    }
-
-    //% block="Traffic Sign Detection detects %sign"
-    //% shim=pixetto::get_traffic_sign
-    export function get_traffic_sign(sign: PixTrafficSign): boolean {
-        return false;
-    }
-
-    //% block="Handwriting Digits Detection detects %digit"
-    //% shim=pixetto::get_hand_digit
-    export function get_hand_digit(digit: PixHandDigit): boolean {
-        return false;
-    }
-
-    //% block="Handwriting Lettes Detection detects %letter"
-    //% shim=pixetto::get_hand_letter
-    export function get_hand_letter(letter: PixHandLetter): boolean {
-        return false;
+    //% block="current function is %type_id"
+    //% shim=pixetto::getTypeID
+    //% weight=89
+    export function getTypeID(): number {
+        return 0;
     }
 
     //% block="object position x"
     //% shim=pixetto::getPosX
+    //% weight=88
     export function getPosX(): number {
         return 0;
     }
     
     //% block="object position y"
     //% shim=pixetto::getPosY
+    //% weight=87
     export function getPosY(): number {
         return 0;
     }
 
     //% block="object width"
     //% shim=pixetto::getWidth
+    //% weight=86
     export function getWidth(): number {
         return 0;
     }
 
     //% block="object height"
     //% shim=pixetto::getHeight
+    //% weight=85
     export function getHeight(): number {
         return 0;
     }
+
+    //% block="Color Detection detects color %color"
+    //% shim=pixetto::get_colordetect_color
+    //% weight=80
+    export function get_colordetect_color(color: PixColorType): boolean {
+        return false;
+    }
     
+    //% block="Shape Detection detects shape %shape"
+    //% shim=pixetto::get_shapedetect_shape
+    //% weight=79
+    export function get_shapedetect_shape(shape: PixShapeType): boolean {
+        return false;
+    }
+
+    //% block="Sphere Detection detects color %color"
+    //% shim=pixetto::get_spheredetect_color
+    //% weight=78
+    export function get_spheredetect_color(color: PixColorType): boolean {
+        return false;
+    }
+
+    //% block="Neural Network detects %id"
+    //% shim=pixetto::get_nn_id
+    //% weight=77
+    export function get_nn_id(id: number): boolean {
+        return false;
+    }
+
+    //% block="Traffic Sign Detection detects %sign"
+    //% shim=pixetto::get_traffic_sign
+    //% weight=76
+    export function get_traffic_sign(sign: PixTrafficSign): boolean {
+        return false;
+    }
+
+    //% block="Handwritten Digits Detection detects %digit"
+    //% shim=pixetto::get_hand_digit
+    //% weight=75
+    export function get_hand_digit(digit: PixHandDigit): boolean {
+        return false;
+    }
+
+    //% block="Handwritten Lettes Detection detects %letter"
+    //% shim=pixetto::get_hand_letter
+    //% weight=74
+    export function get_hand_letter(letter: PixHandLetter): boolean {
+        return false;
+    }
+
+    //% block="Remote Computing detects %id"
+    //% shim=pixetto::get_remote_computing
+    //% weight=73
+    export function get_remote_computing(id: number): boolean {
+        return false;
+    }
+    
+    //% block="Simple Classifier detects %id"
+    //% shim=pixetto::get_simple_classifier
+    //% weight=72
+    export function get_simple_classifier(id: number): boolean {
+        return false;
+    }
+
+    //% block="Voice Command detects %vcmd"
+    //% shim=pixetto::get_voice_command
+    //% weight=71
+    export function get_voice_command(vcmd: PixVoiceCommand): boolean {
+        return false;
+    }
+
+    //% block="Apriltag |%number1"
+    //% shim=pixetto::getApriltagField
+    //% weight=60
+    export function getApriltagField(number1: PixApriltagField): number {
+        return 0;
+    }
+
+    //% block="Lanes Detection |%number1"
+    //% shim=pixetto::getLanesField
+    //% weight=59
+    export function getLanesField(number1: PixLanesField): number {
+        return 0;
+    }
+
     //% block="equation answer"
     //% shim=pixetto::getEquationAnswer
-    export function getEquationAnswer() {
+    //% weight=55
+    export function getEquationAnswer(): number {
         return 0;
     }
 
     //% block="equation expression"
     //% shim=pixetto::getEquationExpr
+    //% weight=54
     export function getEquationExpr() {
         return 0;
     }
-    
 }
--- pixetto.cpp
+++ pixetto.cpp
@@ -17,7 +17,7 @@
 #define PXT_CMD_RESET		0x53 //(83)
 
 #define PXT_RET_CAM_SUCCESS	0xE0
-#define PXT_RET_CAM_ERROR	0xE1
+#define PXT_RET_CAM_ERROR	0xE1   
 
 #define SERIAL_BUF_SIZE		64
 #define DATA_SIZE			33
@@ -41,8 +41,8 @@ enum PixFunction {
         COLOR_LABEL=2,
         //% block="Shape Detection"
         SHAPE_DETECTION=3,
-        //% block="Circle Detection"
-        CIRCLE_DETECTION=4,
+        //% block="Sphere Detection"
+        SPHERE_DETECTION=4,
         //% block="Template Matching"
         TEMPLATE=6,
         //% block="Keypoint"
@@ -55,34 +55,68 @@ enum PixFunction {
         FACE_DETECTION=11,
         //% block="Traffic Sign Detection"
         TRAFFIC_SIGN_DETECTION=12,
-        //% block="Handwriting Digits Detection"
-        HANDWRITING_DIGITS_DETECTION=13,
-        //% block="Handwriting Letters Detection"
-        HANDWRITING_LETTERS_DETECTION=14,
-        /*
+        //% block="Handwritten Digits Detection"
+        HANDWRITTEN_DIGITS_DETECTION=13,
+        //% block="Handwritten Letters Detection"
+        HANDWRITTEN_LETTERS_DETECTION=14,
         //% block="Remote Computing"
         REMOTE_COMPUTING=15,
         //% block="Lanes Detection"
         LANES_DETECTION=16,
-        */
         //% block="Digits Operation"
-        DIGITS_OPERATION=17
-        /*
+        DIGITS_OPERATION=17,
         //% block="Simple Classifier"
         SIMPLE_CLASSIFIER=18,
         //% block="Voice Commands"
         VOICE_COMMANDS=19
-        */
 };
     
+enum PixApriltagField {
+        //% block="position x"
+        APRILTAG_POS_X=1,
+        //% block="position y"
+        APRILTAG_POS_Y,
+        //% block="position z"
+        APRILTAG_POS_Z,
+        //% block="rotation x"
+        APRILTAG_ROT_X,
+        //% block="rotation y"
+        APRILTAG_ROT_Y,
+        //% block="rotation z"
+        APRILTAG_ROT_Z,
+        //% block="center x"
+        APRILTAG_CENTER_X,
+        //% block="center y"
+        APRILTAG_CENTER_Y
+};
+
+enum PixLanesField {
+        //% block="Left X1"
+        LANES_LX1=1,
+        //% block="Left Y1"
+        LANES_LY1,
+        //% block="Left X2"
+        LANES_LX2,
+        //% block="Left Y2"
+        LANES_LY2,
+        //% block="Right X1"
+        LANES_RX1,
+        //% block="Right Y1"
+        LANES_RY1,
+        //% block="Right X2"
+        LANES_RX2,
+        //% block="Right Y2"
+        LANES_RY2
+};
 
 //using namespace pxt;
+//using namespace codal;
 
 namespace pixetto {
-
 	MicroBitSerial *serial = nullptr;
 	uint8_t data_buf[DATA_SIZE] = {0xFF};
 	int data_len = 0;
+	int m_type = 0;
 	int m_x = 0;
 	int m_y = 0;
 	int m_w = 0;
@@ -90,6 +124,8 @@ namespace pixetto {
 	int m_eqLen = 0;
 	float m_eqAnswer = 0;
 	char m_eqExpr[17] = {0};
+	float m_posx=0, m_posy=0, m_posz=0, m_rotx=0, m_roty=0, m_rotz=0, m_centerx=0, m_centery=0;
+	bool bOnStarting = false;
 	
     bool getPinName(PixSerialPin p, PinName& name) {
       switch(p) {
@@ -137,7 +173,7 @@ namespace pixetto {
 
 		return read_idx;
 	}
-
+	
 	bool verifyChecksum(uint8_t *buf, int len)
 	{
 		uint8_t sum = 0;
@@ -150,11 +186,41 @@ namespace pixetto {
 		
 		return (sum == buf[len-2]);
 	}
+	
+	bool checkcam()
+	{
+		ssflush();
+		
+		uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_STREAMON_CB, 0, PXT_PACKET_END};
+		serial->send(cmd_buf, 5, ASYNC);
+			
+		int read_len = 0;
+		int loop = 0;
+		uint8_t code_buf[5] = {0xFF};
+		
+		do {
+			read_len = serial->read(code_buf, 1, ASYNC);
+			
+			if (read_len == 0 || read_len == MICROBIT_NO_DATA) {
+				loop++;
+			}
+		} while (code_buf[0] != PXT_PACKET_START && loop < 50000);
+		
+		if (read_len == 0 || read_len == MICROBIT_NO_DATA) return false;
+			
+		read_len = serial->read(&code_buf[1], 4);
 
+		if (code_buf[0] == PXT_PACKET_START &&
+			code_buf[4] == PXT_PACKET_END &&
+			code_buf[2] == PXT_RET_CAM_SUCCESS)
+			return true;
+		return false;
+	}
+	
 	bool opencam(bool reset) 
 	{
 		if (reset)
-			uBit.sleep(6000);
+			uBit.sleep(8000);
 			
 		int try_streamon = 0;
 		do {
@@ -165,18 +231,23 @@ namespace pixetto {
 			
 			int read_len = 0;
 			uint8_t code_buf[5] = {0xFF};
+			int loop = 0;
 			
-			read_len = ssread(code_buf, 1, 50000);			
+			do {
+				read_len = serial->read(code_buf, 1, ASYNC);
+				loop++;
+			} while (code_buf[0] != PXT_PACKET_START && loop < 300000);
+
 			if (read_len == 0 || read_len == MICROBIT_NO_DATA) return false;
 			
-			read_len = ssread(&code_buf[1], 4, 50000);
+			read_len = serial->read(&code_buf[1], 4);
 
 			if (read_len == 4 &&
 			    code_buf[0] == PXT_PACKET_START &&
 				code_buf[4] == PXT_PACKET_END &&
 				code_buf[2] == PXT_RET_CAM_SUCCESS)
 				return true;
-
+				
 			try_streamon++;
 			uBit.sleep(500);
 		} while (try_streamon < 4);
@@ -185,13 +256,16 @@ namespace pixetto {
 	}
     //% 
     bool begin(PixSerialPin rx, PixSerialPin tx){
-		int ret = false;
+		bOnStarting = true;
+		
+		bool ret = false;
 		PinName txn, rxn;
-		uBit.sleep(8000);
+		uBit.sleep(6000);
+		
 		if (getPinName(tx, txn) && getPinName(rx, rxn))
 		{
 			serial = new MicroBitSerial(txn, rxn, 64, 20);
-
+				
 			#if MICROBIT_CODAL
 			serial->setBaudrate(38400);
 			#else
@@ -199,16 +273,21 @@ namespace pixetto {
 			#endif
 			//serial->setRxBufferSize(64);
 			//serial->setTxBufferSize(32);
-			uBit.sleep(100);
+			uBit.sleep(1000);
 			
 			ret = opencam(false);
 		}
+		if (ret)
+			bOnStarting = false;
+			
 		return ret;
     }
+    
 	//%
     bool isDetected(){
-		int read_len = 0;
-		int loop = 0;
+		if (bOnStarting) 
+			return false;
+		
 		int a = 0;
 		for (a=0; a<DATA_SIZE; a++)
 			data_buf[a] = 0xFF;
@@ -217,29 +296,36 @@ namespace pixetto {
 		uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_QUERY, 0, PXT_PACKET_END};
 		serial->send(cmd_buf, 5, ASYNC);
 
+		int read_len = 0;
+		int loop = 0;
+
 		do {
-			read_len = ssread(data_buf, 1, 50000);
+			read_len = serial->read(data_buf, 1, ASYNC);
 			loop++;
 		} while (data_buf[0] != PXT_PACKET_START && loop < 300000);
 		
 		if (read_len == 0 || read_len == MICROBIT_NO_DATA) {
-			uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_RESET, 0, PXT_PACKET_END};
-			serial->send(cmd_buf, 5, ASYNC);
-			opencam(true);
+			if (!checkcam()) {
+				uint8_t cmd_buf[5] = {PXT_PACKET_START, 0x05, PXT_CMD_RESET, 0, PXT_PACKET_END};
+				serial->send(cmd_buf, 5, ASYNC);
+				opencam(true);
+			}
 			return false;
 		}
 
-		read_len = ssread(&data_buf[1], 2, 50000);
+		read_len = serial->read(&data_buf[1], 2);
 		data_len = data_buf[1];
-		if (data_len > 3)
-			read_len = ssread(&data_buf[3], data_len - 3, 50000);
+		if (data_len > 3) {
+			read_len = serial->read(&data_buf[3], data_len - 3);
+		}
 		else
 			return false;
-
+		
 		if (read_len != (data_len-3)) return false;
 		if (data_buf[data_len-1] != PXT_PACKET_END) return false;
 		if (!verifyChecksum(data_buf, data_len)) return false;
 		if (data_buf[2] == 0) return false; // num == 0
+		
 		
 		if (data_buf[2] == DIGITS_OPERATION) {
 			m_x = data_buf[3];
@@ -259,22 +345,43 @@ namespace pixetto {
 			for (a=0; a<m_eqLen; a++)
 				m_eqExpr[a] = (char)data_buf[a+15];
 		}
-		/*
 		else if (data_buf[2] == LANES_DETECTION) {
+			m_x = data_buf[3];
+			m_y = data_buf[4];
 		}
-		else if (data_buf[2] == APRILTAG) {
+		else if (data_buf[2] == SIMPLE_CLASSIFIER) {
+			m_type = data_buf[3] * 256 + data_buf[4];
+			m_x = data_buf[5];
+			m_y = data_buf[6];
+			m_w = data_buf[7];
+			m_h = data_buf[8];
 		}
-		*/
 		else {
+			m_type = data_buf[3];
 			m_x = data_buf[4];
 			m_y = data_buf[5];
 			m_w = data_buf[6];
 			m_h = data_buf[7];
+			
+			if (data_buf[2] == APRILTAG) {
+				int value = 0;
+				value = (short)(data_buf[8] * 256 + data_buf[9]);
+				m_posx = (float)value / 100.0;
+				value = (short)(data_buf[10] * 256 + data_buf[11]);
+				m_posy = (float)value / 100.0;
+				value = (short)(data_buf[12] * 256 + data_buf[13]);
+				m_posz = (float)value / 100.0;
+				
+				m_rotx = (short)(data_buf[14] * 256 + data_buf[15]);
+				m_roty = (short)(data_buf[16] * 256 + data_buf[17]);
+				m_rotz = (short)(data_buf[18] * 256 + data_buf[19]);
+				
+				m_centerx = (short)(data_buf[20] * 256 + data_buf[21]);
+				m_centery = (short)(data_buf[22] * 256 + data_buf[23]);
+			}
 		}
 		return true;
 	}
-
-
 
 	//%
 	bool getFuncID(int func_id){
@@ -296,8 +403,8 @@ namespace pixetto {
     }
 
     //% 
-    bool get_circledetect_color(int color) {
-		if (data_buf[2] == CIRCLE_DETECTION && data_buf[3] == color)
+    bool get_spheredetect_color(int color) {
+		if (data_buf[2] == SPHERE_DETECTION && data_buf[3] == color)
 			return true;
         return false;
     }
@@ -318,18 +425,43 @@ namespace pixetto {
 
     //%
     bool get_hand_digit(int digit) {
-		if (data_buf[2] == HANDWRITING_DIGITS_DETECTION && data_buf[3] == digit)
+		if (data_buf[2] == HANDWRITTEN_DIGITS_DETECTION && data_buf[3] == digit)
 			return true;
         return false;
     }
 
     //%
     bool get_hand_letter(int letter) {
-		if (data_buf[2] == HANDWRITING_LETTERS_DETECTION && data_buf[3] == letter)
+		if (data_buf[2] == HANDWRITTEN_LETTERS_DETECTION && data_buf[3] == letter)
 			return true;
         return false;
     }	
+
+    //%
+    bool get_remote_computing(int id) {
+		if (data_buf[2] == REMOTE_COMPUTING && data_buf[3] == id)
+			return true;
+        return false;
+    }
 	
+    //%
+    bool get_simple_classifier(int id) {
+		if (data_buf[2] == SIMPLE_CLASSIFIER && data_buf[3] == id)
+			return true;
+        return false;
+    }
+
+    //%
+    bool get_voice_command(int vcmd) {
+		if (data_buf[2] == VOICE_COMMANDS && data_buf[3] == vcmd)
+			return true;
+        return false;
+    }
+
+	//%
+	int getTypeID() {
+		return m_type;
+	}
 	//%
 	int getPosX() {
 		return m_x;
@@ -358,4 +490,51 @@ namespace pixetto {
 		return PSTR(s);
 	}
 	
+	//%
+	float getApriltagField(int field) {
+		switch(field) {
+			case APRILTAG_POS_X:
+				return m_posx;
+			case APRILTAG_POS_Y:
+				return m_posy;
+			case APRILTAG_POS_Z:
+				return m_posz;
+			case APRILTAG_ROT_X:
+				return m_rotx;
+			case APRILTAG_ROT_Y:
+				return m_roty;
+			case APRILTAG_ROT_Z:
+				return m_rotz;
+			case APRILTAG_CENTER_X:
+				return m_centerx;
+			case APRILTAG_CENTER_Y:
+				return m_centery;
+			default:
+				return 0;
+		}
+	}
+		
+	//%
+	int getLanesField(int field) {
+		switch(field) {
+			case LANES_LX1:
+				return data_buf[5];
+			case LANES_LY1:
+				return data_buf[6];
+			case LANES_LX2:
+				return data_buf[7];
+			case LANES_LY2:
+				return data_buf[8];
+			case LANES_RX1:
+				return data_buf[9];
+			case LANES_RY1:
+				return data_buf[10];
+			case LANES_RX2:
+				return data_buf[11];
+			case LANES_RY2:
+				return data_buf[12];
+			default:
+				return 0;
+		}
+	}
 }
